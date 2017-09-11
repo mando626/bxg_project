d7cd24de002fd6676c8365dd6f0905438840687d
mando <991768079@qq.com>
2017-09-11 21:51:13 +0800

    第二次 >  模块化的登陆页面和首页 > 01-将js.html的引入进行尝试用requirejs > 02-修改后发现登陆页面也是不断的刷新进入死循环，利用判断（location的全局属性pathname） > 03-。。。待续 master

---
+++ views/static/js/lib/config.js
@@ -0,0 +1,13 @@
+require.config({
+  //baseUrl设置的原则，就是大部分资源共享的目录
+  baseUrl:"/views/assets",
+
+  paths:{
+    //
+    jquery:"./jquery/jquery",
+    cookie:"./jquery-cookie/jquery.cookie",
+    template:"./artTemplate/template"
+  }
+
+
+})
